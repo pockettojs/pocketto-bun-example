1ada7c704404122723dd7dcb0655b43cf5962b31
Kent Ng <kent.ng201@gmail.com>
2024-10-27 14:53:36 +0800

    install pocketto, add use case

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
 import { Elysia } from "elysia";
+import { DatabaseManager } from "pocketto";
+import { SalesInvoice } from "./models/slave/SalesInvoice.p";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+async function connectDb() {
+    await DatabaseManager.connect('http://localhost:5984/test', {
+        dbName: 'remote',
+        silentConnect: true,
+        auth: {
+            username: 'admin',
+            password: 'qwer1234'
+        }
+    });
+}
+
+const app = new Elysia()
+    .get("/", () => 'ok')
+    .get("/sales-invoices", async () => {
+        await connectDb();
+        return SalesInvoice.via('remote').get().then((items) => items.map((item) => item));
+    })
+    .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
